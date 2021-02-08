7a3231c582a22bfc6e499d8dce7634a93599519e
Gourav-raj <gouravrj9654@gmail.com>
2021-02-08 18:09:12 +0530

    Adding Site

---
+++ src/Components/modal.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+  render() {
+    const style = {
+        overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.75)'
+        }
+    };
+        return (
+            <ReactModal className='popup-modal mfp-hide' isOpen={this.props.isOpen} onRequestClose={this.props.onRequestClose} style={style}>
+                <img
+                    className='scale-with-grid'
+                    src={this.props.entry.image.modal}
+                    alt={this.props.entry.name}/>
+                <div className='description-box'>
+                    <h5>{this.props.entry.name}</h5>
+                    <p>{this.props.entry.summary}</p>
+                    <span className='categories'>
+                        <i className='fa fa-tag'/>
+                        {this.props.entry.keywords.join(', ')}
+                    </span>
+                </div>
+                <div className='link-box'>
+                    <a
+                        href={this.props.entry.website}
+                        target='_blank'
+                        rel='noopener noreferrer'>
+                        Details
+                    </a>
+                    <a className='popup-modal-dismiss' onClick={this.props.onRequestClose}>Close</a>
+                </div>
+            </ReactModal>
+        );
+    }
+}
+
+export default Modal;
